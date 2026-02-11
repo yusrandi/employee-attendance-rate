9b130664d3b8882f49c712964a1cf31eec632c23
yusrandi <yusrandhyr@gmail.com>
2026-02-12 07:29:00 +0800

    revisi 2a

--- src/features/attendance/AttendanceDistributionChart.tsx
+++ src/features/attendance/AttendanceDistributionChart.tsx
@@ -1,5 +1,5 @@
 // components/AttendanceDistributionChart.tsx
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { PieChart, Pie, Cell } from 'recharts'
 import {
   Card,
